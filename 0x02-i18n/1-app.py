04d35d70d3f67536700bf44d5e20fb8c358f85bf
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-15 00:57:13 +0300

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,12 +3,24 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """index func"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
