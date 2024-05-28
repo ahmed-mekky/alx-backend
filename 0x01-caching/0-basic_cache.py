433c5c6e57efcad2bcb441e15fdc58a1b62c76c3
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-29 02:32:23 +0300

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" BasicCache module"""
+from basic_cache import BaseCaching
+
+class BasicCache (BaseCaching):
+    """Basic Cache"""
+
+    def put(self, key, item):
+        """puts an item"""
+        if item and key:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """gets an item"""
+        if key:
+            return self.cache_data.get(key)
