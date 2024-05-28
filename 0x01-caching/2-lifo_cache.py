4fef111883fd4ed9a3d388f981b6b884f751c703
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-29 02:56:04 +0300

    caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" BasicCache module"""
+from base_caching import BaseCaching
+
+
+class LIFOCache (BaseCaching):
+    """lifo cashing"""
+
+    def put(self, key, item):
+        """puts an item"""
+        if item and key:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                self.cache_data.popitem()
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """gets an item"""
+        if key:
+            return self.cache_data.get(key)
