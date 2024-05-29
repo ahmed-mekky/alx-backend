8b166d880d1f0f618f47bdc3bd8ef5d278161c98
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-29 03:00:02 +0300

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,11 +9,11 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """puts an item"""
         if item and key:
+            self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 first_key = list(self.cache_data.keys())[0]
                 self.cache_data.pop(first_key)
                 print(f'DISCARD: {first_key}')
-            self.cache_data[key] = item
 
     def get(self, key):
         """gets an item"""
